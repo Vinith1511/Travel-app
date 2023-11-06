96d5f085a3b9d6a30a5cee4c65cd0ca4d23b6f0a
Vinith1511 <vinithkumar4731@gmail.com>
2023-11-06 12:43:59 +0530

    update

--- src/componnet/roomDetails/RoomBookingDetails.js
+++ src/componnet/roomDetails/RoomBookingDetails.js
@@ -3,9 +3,9 @@ import "./RoomBookingDetails.css"
 function RoomBookingDetails({ car, closeModal }) {
   const [bookingDetails, setBookingDetails] = useState(null);
   const [bookingNum, setBookingNum] = useState(generateRandomBookingNumber());
-    function generateRandomBookingNumber() {
-        return Math.floor(Math.random() * 9999 + 9999);
-      }
+  function generateRandomBookingNumber() {
+    return Math.floor(Math.random() * 9999 + 9999);
+  }
   useEffect(() => {
     const storedDetails = localStorage.getItem('roomBookingsData');
 
@@ -16,21 +16,21 @@ function RoomBookingDetails({ car, closeModal }) {
   function handleButtonClick() {
     setBookingNum(generateRandomBookingNumber());
   }
-  console.log(bookingDetails,'BOOK')
+  console.log(bookingDetails, 'BOOK')
   return (
-    <div className='booking-container 'style={{ height: '444px' }} >
-        <h2>Booking Details</h2>
-{bookingDetails?.map((e,index) => (
-  <div>
-    <p id="count">Booking number:{bookingNum}</p>
+    <div className='booking-container ' style={{ height: '444px' }} >
+      <h2>Booking Details</h2>
+      {bookingDetails?.map((e, index) => (
+        <div>
+          <p id="count">Booking number:{bookingNum}</p>
 
-    <p>Check-In: {e.checkin}</p>
-      <p>Check-Out: {e.checkout}</p>
-      <p>Aadhar: {e.aadhar}</p>
-      <p>Phone: {e.phone}</p>
-      <p>Person: {e.persons}</p>
-  </div>
-))}
+          <p>Check-In: {e.checkin}</p>
+          <p>Check-Out: {e.checkout}</p>
+          <p>Aadhar: {e.aadhar}</p>
+          <p>Phone: {e.phone}</p>
+          <p>Person: {e.persons}</p>
+        </div>
+      ))}
     </div>
   );
 }
